732eef1766e36f4ae596291443845a3afd59356c
Edem Akato <akatoaugustine@gmail.com>
2023-09-27 22:10:36 +0000

    Repositories

--- src/main/java/com/edem/securecapita/repository/UserRepository.java
+++ src/main/java/com/edem/securecapita/repository/UserRepository.java
@@ -2,7 +2,14 @@ package com.edem.securecapita.repository;
 
 import com.edem.securecapita.model.User;
 
+import java.util.Collection;
 
 
 public interface UserRepository <T extends User>{
+    T create(T data);
+    Collection<T> list ( int page, int pageSize);
+
+    T get(Long id);
+    T update (T data);
+    Boolean delete(Long id);
 }
