7412e5c532f28f0a788bde0c846c9cc7e0787e58
Edem Akato <akatoaugustine@gmail.com>
2023-09-27 17:48:28 +0000

    Sql tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -81,10 +81,21 @@ DROP TABLE IF EXISTS AccountVerifications;
 CREATE TABLE AccountVerifications (
     id       BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     user_id  BIGINT UNSIGNED NOT NULL ,
-    url      VARCHAR(255) DEFAULT NULL,
+    url      VARCHAR(255) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT UQ_AccountVerifications_Url UNIQUE (url),
     CONSTRAINT UQ_AccountVerifications_User_id  UNIQUE (user_id)
+);
+
+DROP TABLE IF EXISTS ResetPasswordVerifications;
+CREATE TABLE ResetPasswordVerifications(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL ,
+    url VARCHAR(255) NOT NULL ,
+    expiration_date DATETIME NOT NULL ,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT UQ_ResetPasswordVerifications_Url UNIQUE (url),
+    CONSTRAINT UQ_ResetPasswordVerifications_User_id UNIQUE (user_id)                                      
 );
 
 
