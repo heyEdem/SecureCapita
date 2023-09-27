d54a8bf4204c4213b8dc344601fc01a893df2319
Edem Akato <akatoaugustine@gmail.com>
2023-09-27 20:34:22 +0000

    Sql tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -95,8 +95,33 @@ CREATE TABLE ResetPasswordVerifications(
     expiration_date DATETIME NOT NULL ,
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT UQ_ResetPasswordVerifications_Url UNIQUE (url),
-    CONSTRAINT UQ_ResetPasswordVerifications_User_id UNIQUE (user_id)                                      
+    CONSTRAINT UQ_ResetPasswordVerifications_User_id UNIQUE (user_id)
 );
+
+
+DROP TABLE IF EXISTS TwoFactorVerifications;
+CREATE TABLE TwoFactorVerifications(
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    code VARCHAR(10) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT UQ_TwoFactorVerifications_User_id UNIQUE (user_id),
+    CONSTRAINT UQ_TwoFactorVerifications_Code UNIQUE (code)
+s
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
