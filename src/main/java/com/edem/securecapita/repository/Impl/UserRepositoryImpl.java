ead294242c2bcd9601e73bd114a2fd7ce4c9bd14
Edem Akato <akatoaugustine@gmail.com>
2023-09-28 00:15:29 +0000

    Repositories

---
+++ src/main/java/com/edem/securecapita/repository/Impl/UserRepositoryImpl.java
@@ -0,0 +1,73 @@
+package com.edem.securecapita.repository.Impl;
+
+import com.edem.securecapita.exception.ApiException;
+import com.edem.securecapita.model.User;
+import com.edem.securecapita.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Map;
+
+import static com.edem.securecapita.query.UserQuery.*;
+
+import static java.util.Objects.requireNonNull;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class UserRepositoryImpl implements UserRepository<User> {
+
+    private NamedParameterJdbcTemplate jdbc;
+
+    @Override
+    public User create(User user) {
+        if (getEmailCount(user.getEmail().trim().toLowerCase())> 0) throw new ApiException("Email already exists with another account");
+        try{
+            KeyHolder holder = new GeneratedKeyHolder();
+            SqlParameterSource parameters = getSqlParameterSource(user);
+            jdbc.update(INSERT_USER_QUERY, parameters, holder);
+            user.setId(requireNonNull(holder.getKey()).longValue());
+            roleRepository.addRoleToUser(user.getId(), ROLE_USER.name());
+        }catch (EmptyResultDataAccessException exception) {
+
+        } catch (Exception exception){}()
+        return null;
+    }
+
+
+
+
+    @Override
+    public Collection<User> list(int page, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public User get(Long id) {
+        return null;
+    }
+
+    @Override
+    public User update(User data) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+
+    private Integer getEmailCount(String email) {
+        return jdbc.queryForObject(COUNT_USER_EMAIL_QUERY, Map.of("email", email), Integer.class);
+    }
+
+    private SqlParameterSource getSqlParameterSource(User user) {
+    }
+}
